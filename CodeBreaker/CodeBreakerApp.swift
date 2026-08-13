5033068f4dc54322322b26307ab9ce330e05bbbe
myonixx <myonixx@gmail.com>
2026-08-13 02:57:27 -0700

    initial commit of app files

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by JD on 8/13/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CodeBreakerView()
+        }
+    }
+}
---
+++ CodeBreaker/CodeBreakerView.swift
@@ -0,0 +1,18 @@
+//
+//  CodeBreakerView.swift
+//  CodeBreaker
+//
+//  Created by JD on 8/13/26.
+//
+
+import SwiftUI
+
+struct CodeBreakerView: View {
+    var body: some View {
+        Text("Code Breaker")
+    }
+}
+
+#Preview {
+    CodeBreakerView()
+}
